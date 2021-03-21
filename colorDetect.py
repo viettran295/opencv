69a5830d6df7c781e6f218b6718d102d39f85f9d
viet.tran <viettran295@gmail.com>
2021-03-21 09:18:05 +0100

    add red detect function

--- colorDetect.py
+++ colorDetect.py
@@ -5,16 +5,25 @@ import random
 cap = cv2.VideoCapture(0)
 
 while True: 
-    ret, frame = cap.read()
-    
+    _, frame = cap.read()
+
     img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([110,50,50])
+    
+    #blue detect 
+    lower_blue = np.array([90,50,50])
     upper_blue = np.array([130,255,255])
 
-    mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    #red detect 
+    lower_red = np.array([150,150,50])
+    upper_red = np.array([180,255,150])
 
-    cv2.imshow("hsv", res)
+    mask_blue = cv2.inRange(img_hsv, lower_blue, upper_blue)
+    mask_red = cv2.inRange(img_hsv, lower_red, upper_red)
+    res_blue = cv2.bitwise_and(frame, frame, mask=mask_blue)
+    res_red = cv2.bitwise_and(frame, frame, mask= mask_red)
+
+    cv2.imshow("hsv", res_blue)
+    cv2.imshow("video", res_red)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
--- nump.py
+++ nump.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np 
 
-blue = np.array([110,50,50])
-print(blue)
+img = cv2.imread("nguynmap.jpg")
+img = cv2.rectangle(img, (320,320),(600,600),(114,188,212), -1)
+
+cv2.imshow("img",img)
+cv2.waitKey(0)
