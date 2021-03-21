d0a311bcc30a111e0efd4ed32e9c03a02c48feb6
viet.tran <viettran295@gmail.com>
2021-03-21 11:38:45 +0100

    change folder

--- nump.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np 
-
-img = cv2.imread("nguynmap.jpg")
-img = cv2.rectangle(img, (320,320),(600,600),(114,188,212), -1)
-
-cv2.imshow("img",img)
-cv2.waitKey(0)
---
+++ test.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np 
+
+img = cv2.imread("Lenna.png")
+
+width = int(img.shape[0]*50/100)
+height = int(img.shape[1]*50/100)
+dim = (width, height)
+
+img_resize = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+img_hsv = cv2.cvtColor(img_resize, cv2.COLOR_BGR2HSV)
+img_hsv = cv2.rectangle(img_resize, (0,0), (100,100), (0,150,150), -1)
+img_cat = np.concatenate((img_resize, img_hsv), axis=1)
+
+cv2.imshow("cat", img_cat)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
