49520948e165ee4b56ac80911dbdc0ab070705ec
viet.tran <viettran295@gmail.com>
2021-03-22 10:58:31 +0100

    add color name func

--- Palette/paletteDetect.py
+++ Palette/paletteDetect.py
@@ -1,11 +1,24 @@
 import cv2 
 import numpy as np
+import pandas as pd
 
 img = cv2.imread("palette.jpg")
-
 palette = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+
+index = ["color","color_name","hex","R","G","B"]
+csv = pd.read_csv("colors.csv", names=index)
+
 clicked = False
 b = g = r = x_pos = y_pos = 0
+
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R-int(csv.loc[i,"R"])) + abs(G-int(csv.loc[i,"G"]))+ abs(B-int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
 def draw_fucntion(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         global r, g, b, clicked, x_pos, y_pos
@@ -16,13 +29,16 @@ def draw_fucntion(event,x,y,flags,params):
         b = int(b)
         r = int(r)
         g = int(g)
+        print(f"b = {b}, r = {r}, g = {g}")
 
 cv2.namedWindow("palette")
 cv2.setMouseCallback("palette", draw_fucntion)
 while True: 
     cv2.imshow("palette", palette)
-    if(clicked == True):
+    if(clicked):
         cv2.rectangle(palette, (10,20),(750,60),(b,r,g),-1)
+        text = getColorName(r,g,b)
+        cv2.putText(palette,text, (50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
         clicked = False
     if cv2.waitKey(1) == ord('q'):
         break
--- Palette/tempCodeRunnerFile.py
+++ Palette/tempCodeRunnerFile.py
@@ -1,6 +1,3 @@
-def draw(event,x,y,flags,params):
-#     if event == cv2.EVENT_LBUTTONDBLCLK:
-#         global r,g,b,clicked,x_pos,y_pos
-#         x_pos = x
-#         y_pos = y
-#         b, r, g = int(palette[x][y])
+    R = 0
+    G = 66
+    B = 66
