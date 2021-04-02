2ec65042ee27ea6bbfdf8b2b3375c4af80367387
viet.tran <viettran295@gmail.com>
2021-04-02 16:44:17 +0200

    getMaxContour() works

--- handTrack/fingerCountcv.py
+++ handTrack/fingerCountcv.py
@@ -12,53 +12,47 @@ def preprocess(img):
     mask = cv2.inRange(img_hsv,lower,upper)
     # hand = cv2.bitwise_and(img,img, mask=mask)
     return mask
+
 def threshhold(mask):   
     blurred = cv2.blur(mask,(2,2))
     ret, thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY)
     return thresh
-    
-def getMaxContour(img,thresh):
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = max(contours, key=lambda x: cv2.contourArea(x))
-    hull = cv2.convexHull(contours)
-    cv2.drawContours(img, [hull], -1, (0,255,255),2)
-    cv2.drawContours(img, [contours], -1, (255,0,0), 2)
-    return img
-    # defects = cv2.convexityDefects(contours,hull)
-    # if defects is not None:
-    #     print("defect")
 
 def calculateAngle(far,start,end):
     #cosine rule
-    a = math.sqrt((end[0]-start[0])**2+(end[0]-start[0])**2)
-    b = math.sqrt((far[0]-start[0])**2+(far[0]-start[0])**2)
-    c = math.sqrt((end[0]-far[0])**2+(end[0]-far[0])**2)
-    angle = math.acos((b**2+c**2-a**2)/(2*b*c))
+    a = math.sqrt((end[0]-start[0])**2+(end[1]-start[1])**2)
+    b = math.sqrt((far[0]-start[0])**2+(far[1]-start[1])**2)
+    c = math.sqrt((end[0]-far[0])**2+(end[1]-far[1])**2)
+    angle = math.acos((b**2 + c**2 - a**2)/(2*b*c))
     return angle
 
-# def countFinger(contour):
-#     hull = cv2.convexHull(contour)
-#     if len(hull)>3:
-#         defects = cv2.convexityDefects(contour, hull)
-#         cnt = 0
-#         if defects is not None:
-#             for i in range(defects.shape[0]):
-#                 s,e,f,d = defects[i,0]
-#                 start = tuple(contour[s,0])
-#                 end = tuple(contour[e,0])
-#                 far = tuple(contour[f,0])
-#                 angle = calculateAngle(far,start,end)
-#                 if d>1000 and angle<=math.pi/2:
-#                     cnt+=1
-#         return cnt
-#     return 0
+def getMaxContour(img,thresh):
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    #choose and draw max Area
+    contours = max(contours, key=lambda x: cv2.contourArea(x))
+    hull = cv2.convexHull(contours, returnPoints=False)
+    # cv2.drawContours(img, [hull], -1, (0,255,255),2)
+    cv2.drawContours(img, [contours], -1, (255,0,0), 1)
+    defects = cv2.convexityDefects(contours, hull)
+    if defects is not None:
+        cnt = 0
+        for i in range(defects.shape[0]):
+            s,e,f,d = defects[i,0]
+            start = tuple(contours[s,0])
+            end = tuple(contours[e,0])
+            far = tuple(contours[f,0])
+            angle = calculateAngle(far,start,end)
+            if angle<=math.pi/2:
+                cnt+=1
+    cv2.putText(img,str(cnt),(0,100),cv2.FONT_HERSHEY_COMPLEX,4,(255,255,0),2,cv2.LINE_AA)            
+    return img
 
 while True:
     _,frame = cap.read()
     mask = preprocess(frame)
     thresh = threshhold(mask)
     contour = getMaxContour(frame,thresh)
-    
+
     cv2.imshow("origin",contour)
     cv2.imshow("thresh",thresh)
 
--- handTrack/test.py
+++ handTrack/test.py
@@ -1,28 +1,23 @@
 import cv2
-import mediapipe as mp 
+import numpy as np
 
 cap = cv2.VideoCapture(0)
-mp_hand = mp.solutions.hands
-hands = mp_hand.Hands()
-mp_draw = mp.solutions.drawing_utils
+def preprocess(img):
+    img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    lower = np.array([0,48,80])
+    upper = np.array([20,255,255])
+    mask = cv2.inRange(img_hsv,lower,upper)
+    ret,thresh = cv2.threshold(mask,0,255,cv2.THRESH_BINARY)
+    return thresh
 
 while True:
     _, frame = cap.read()
-    imgRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    res = hands.process(imgRGB)
-    #if result contain value of landmarks
-    if res.multi_hand_landmarks:
-        for hand in res.multi_hand_landmarks:
-            #position x,y,z of hand
-            print(hand)
-            mp_draw.draw_landmarks(frame,hand,mp_hand.HAND_CONNECTIONS)
-            #convert x,y,z to pixel 
-            for id, lm in enumerate(hand.landmark):
-                h,w,c = frame.shape
-                cx,cy = int(lm.x*w), int(lm.y*h)
-                print(id,cx,cy)
-
-    cv2.imshow("frame",frame)
+    thresh = preprocess(frame)
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    contours = max(contours, key= lambda x: cv2.contourArea(x))
+    cv2.drawContours(frame,[contours], -1, (255,0,0), 2)
+    cv2.imshow("origin",frame)
+    cv2.imshow("frame",thresh)
     if cv2.waitKey(1) == ord('q'):
         break
 cv2.destroyAllWindows()
